dba5a819d87cc9e0f99933dcc12e15d165f6bd28
Raul Pérez Hinojal <raul.perez@gracia.lasalle.cat>
2025-12-28 11:24:00 +0100

    configuracion inicial i importo las librerias necesarias

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,106 @@ f f f c 3 c c c c 3 c f f f
 . . . . f f f f f f . . . . 
 . . . . f f . . f f . . . . 
 `;
+            case "image3":
+            case "miImagen":return img`
+.............6666...............
+..........666667766.6666........
+.........677777777767776........
+......66667775577757777666......
+.....677666675557557776777666...
+.....6776777775555577777766776..
+...66666777777775777777766666...
+.66667767777755757555777776776..
+6666777677775577557555777767766.
+.6667767777777775577777777767666
+.c6766777677777775777777677766..
+cc77666667777777777777777666666c
+cc76666677777777777777777766776c
+c6666776777777777777766677666776
+66667766667776777767767766766666
+ccc76677677776677766767776776ccc
+cc7766776777677677676667767766cc
+.666c676667677766667766666666cc.
+.ccc66676666776666677677666cccc.
+...ccc77c6767666676676677666ccc.
+...cc676c7766676677666666c666cc.
+....c6cc676c6677677c66c666ccc...
+....ccccc6c66667667cc6ccc6ccc...
+......ccccc66c66c66cccccccc.....
+.......cc.cc6c6ccc6cccc.cc......
+...........cccccccccc...........
+.............feeeeee............
+............feeeeeefe...........
+.........eeeeefeeeffee..........
+............ffffeef..ee.........
+...............fee..............
+................e...............
+`;
+            case "image4":
+            case "miImagen2":return img`
+. . . . f f f f . . . . . 
+. . f f f f f f f f . . . 
+. f f f f f f c f f f . . 
+f f f f f f c c f f f c . 
+f f f c f f f f f f f c . 
+c c c f f f e e f f c c . 
+f f f f f e e f f c c f . 
+f f f b f e e f b f f f . 
+. f 4 1 f 4 4 f 1 4 f . . 
+. f e 4 4 4 4 4 4 e f . . 
+. f f f e e e e f f f . . 
+f e f b 7 7 7 7 b f e f . 
+e 4 f 7 7 7 7 7 7 f 4 e . 
+e e f 6 6 6 6 6 6 f e e . 
+. . . f f f f f f . . . . 
+. . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "miImagen1":return img`
+..cccccccccccccccccccccccccccc..
+.bddddddddddddddddddddddddddddb.
+cddddddddddddddddddddddddddddddc
+cbbb3ddd33d3dddd3333dddd3d333bbc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cbb33dddd3bb33d33dd33ddd33333bbc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cb333dddd3db3dddddddd33333ddd3bc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cddddddddddddddddddddddddddddddc
+cbbbbbb3333333dddd333d3dddd33bbc
+cddddddddddddddddddddddddddddddc
+cbddddddddddddddddddddddddddddbc
+cbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbc
+cbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbc
+.cccccccccccccccccccccccccccccc.
+..cbbc....................cbbc..
+..c33c....................c33c..
+...cc......................cc...
+`;
+            case "image5":
+            case "miImagen0":return img`
+5 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 
+7 7 7 5 7 5 5 7 7 7 7 7 5 7 7 7 
+7 7 6 5 5 7 5 7 5 5 7 7 7 7 7 7 
+7 7 7 6 5 7 7 5 5 6 7 7 7 7 7 7 
+7 7 7 7 6 7 7 5 6 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 5 5 7 7 7 
+7 7 7 7 7 7 7 7 7 7 5 5 6 7 7 7 
+7 7 7 7 7 7 7 7 5 5 7 6 7 7 7 7 
+7 7 7 7 7 7 7 7 7 5 5 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 5 
+7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 5 
+`;
         }
         return null;
     })
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "map":
+            case "level1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020404040404040404040404040104020204010101010101010101010101040202040101010101010101010101010402020401010101010101010101010104040204010101010101010101010101040402040101010101010101010101010402020401010303030303030303010104020204010103030303030303030101040202010101010101010101010101040402010101010101010101040101010404020101010101010101010101010104040202010101010101010101010401040402020101010101010101010101010404020201010101010101040101010404040202020202020202020202020202020202`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.castle.tilePath5,sprites.castle.tileGrass1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
